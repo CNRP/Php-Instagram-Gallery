ec259f6d7110cb579ec641c02908c72e283e533f
CNRP <49189988+CNRP@users.noreply.github.com>
2023-01-28 00:46:44 +0000

    Update insta.py
    removed old code

--- insta.py
+++ insta.py
@@ -1,39 +1,3 @@
-# from itertools import islice
-# from math import ceil
-
-# from instaloader import Instaloader, Profile
-
-# PROFILE = "everton"        # profile to download from
-# X_percentage = 10    # percentage of posts that should be downloaded
-
-# L = Instaloader()
-
-# profile = Profile.from_username(L.context, PROFILE)
-# posts_sorted_by_likes = sorted(profile.get_posts(),
-#                                key=lambda p: p.likes + p.comments,
-#                                reverse=True)
-
-# # for post in islice(posts_sorted_by_likes, ceil(profile.mediacount * X_percentage / 100)):
-# #     L.download_post(post, PROFILE)
-
-# import instaloader
-
-# # Get instance
-# L = instaloader.Instaloader()
-
-# USER = "";
-# PASSWORD = "!";
-
-# # Optionally, login or load session
-# # L.login(USER, PASSWORD)        # (login)
-# # L.interactive_login(USER)      # (ask password on terminal)
-# # L.load_session_from_file(USER) # (load session created w/
-# #                                #  `instaloader -l USERNAME`)
-# profile = instaloader.Profile.from_username(L.context, "everton")
-# for post in profile.get_posts():
-#     L.fast_update = True
-#     L.download_post(post, target=profile.username)
-
 from datetime import datetime
 from itertools import dropwhile, takewhile
 
